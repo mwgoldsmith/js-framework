a7b4bc16a45ef93f44fc34397233a84b3f5dcfcf
Michael Goldsmith <mgoldsmith@mdsol.com>
2013-09-17 19:01:09 -0400

    qunit

---
+++ src/ui.js
@@ -0,0 +1,18 @@
+﻿/*global namespace*/
+// @DONE (2013-09-17 12:22)
+define([
+    './core'
+], function (mdsol) {
+    /*
+    * Use IIFE to prevent cluttering of globals
+    */
+    (function () {
+        function dispose() {
+            return mdsol;
+        }
+
+        namespace('mdsol.ui', {
+            dispose: dispose
+        });
+    } ());
+}); 
