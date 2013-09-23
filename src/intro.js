8bf1d55af78a56dad8e6a00c1ef420ab7c6ce461
Michael Goldsmith <mgoldsmith@mdsol.com>
2013-09-23 19:39:35 -0400

    Removed unnecessary files

--- src/intro.js
+++
@@ -1,9 +0,0 @@
-// @DONE (2013-09-17 09:43)
-(function($, undefined) {
-    /*
-    * Some versions of JScript fail to enumerate over properties, names of which 
-    * correspond to non-enumerable properties in the prototype chain. IE6 doesn't
-    * enumerate `toString` and `valueOf` (among other built-in `Object.prototype`)
-    * properties.
-    */
-    var DONT_ENUM_BUG = !({ toString: null }).propertyIsEnumerable('toString');
